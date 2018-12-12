67e2f7cc91b4845c701c378e2d8276c772ac655d
orelmuseri1 <orelmuseri1@h-mbp-sl-orel>
2018-12-12 20:07:03 +0200

    open and save file

--- PipeGame Gui/src/view/MainWindowController.java
+++ PipeGame Gui/src/view/MainWindowController.java
@@ -1,12 +1,24 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
 
-public class MainWindowController implements Initializable{
+public class MainWindowController implements Initializable,Observer{
 	@FXML
 	MazeDisplayer mazeDisplayer;
 	char[][] mazeData= {
@@ -16,10 +28,97 @@ public class MainWindowController implements Initializable{
 			{'F','-','|','L','-'},
 			{' ','j','|','L','-'},
 	};
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-	mazeDisplayer.setMazeData(mazeData);
+		mazeDisplayer.setMazeData(mazeData);
 	}
-	
+
+	public void start() {
+		System.out.println("Start.");
+	}
+
+	public void stop() {
+		System.out.println("Stop.");
+	}
+
+	public void exit() {
+		System.out.println("Exiting..");
+		System.exit(0);
+	}
+
+	public void openFile() {
+		System.out.println("Open File.");
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Open Pipe File");
+		fc.setInitialDirectory(new File("./resources"));
+
+		FileChooser.ExtensionFilter txtExtensionFilter = new FileChooser.ExtensionFilter("Text Files", "*.txt");
+		fc.getExtensionFilters().add(txtExtensionFilter);
+		fc.setSelectedExtensionFilter(txtExtensionFilter);
+		File choosen = fc.showOpenDialog(null);
+
+		if (choosen != null) {
+			System.out.println(choosen.getName());
+
+			List<char[]> lines = new ArrayList<char[]>();
+			BufferedReader reader;
+			try {
+				reader = new BufferedReader(new FileReader(choosen));
+
+				String line;
+				while ((line = reader.readLine()) != null) {
+					lines.add(line.toCharArray());
+				}
+				reader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			char[][] charArray = lines.toArray(new char[lines.size()][]);
+			mazeDisplayer.setMazeData(charArray);
+		}
+	}
+
+	public void saveFile() {
+		System.out.println("Saving into File.");
+		FileChooser fc = new FileChooser();
+		fc.setTitle("Choose location To Save File");
+		FileChooser.ExtensionFilter txtExtensionFilter = new FileChooser.ExtensionFilter("Text Files", "*.txt");
+		fc.getExtensionFilters().add(txtExtensionFilter);
+		fc.setSelectedExtensionFilter(txtExtensionFilter);
+
+		File selectedFile = null;
+		selectedFile = fc.showSaveDialog(null);
+
+		if (selectedFile == null) {
+			return;
+		}
+
+		PrintWriter outFile = null;
+		try {
+			outFile = new PrintWriter(selectedFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < mazeData.length; i++) {
+			for (int j = 0; j < mazeData[i].length; j++) {
+				outFile.print(mazeData[i][j]);
+			}
+			outFile.println();
+		}
+
+		outFile.close();
+	}
+
+	@Override
+	public void update(java.util.Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
 }
