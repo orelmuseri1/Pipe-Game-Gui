1863486a2f71424fc82a20b5cc679af73f7bdca2
Gon Lubel <greengon1991@gmail.com>
2018-12-14 19:37:57 +0200

    Started to work on the model as requsted by the project 5 road stone.

--- PipeGame Gui/src/model/PipeGameModel.java
+++ PipeGame Gui/src/model/PipeGameModel.java
@@ -4,7 +4,40 @@ import java.util.Observable;
 
 public class PipeGameModel extends Observable implements gameModel {
 
-	char[][] gameSolution;
+	int n; // Size of the board.
+	int numOfMovedPlayed; // Number of moved played by a given player.
+	char[][] pipeBoardState; // Current state of the board.
+	char[][] gameSolution; // The game end solution given by the server.
+
+	public PipeGameModel(int boardSize,char[][] board) {
+		// TODO Auto-generated constructor stub
+		this.n= boardSize;
+		this.numOfMovedPlayed = 0;
+		for (int i = 0 ; i < n ; ++i)
+			for (int j =0 ; j < n ; ++j) {
+				this.gameSolution[i][j] = board[i][j];
+			}
+		this.gameSolution = getSolutionFromServer(this.pipeBoardState);
+	}
+	
+	public PipeGameModel(int boardSize,char[][] board,int numberOfmovesAlreadyPlayed) {
+		// TODO Auto-generated constructor stub
+		this.n= boardSize;
+		this.numOfMovedPlayed = numberOfmovesAlreadyPlayed;
+		for (int i = 0 ; i < n ; ++i)
+			for (int j =0 ; j < n ; ++j) {
+				this.gameSolution[i][j] = board[i][j];
+			}
+		this.gameSolution = getSolutionFromServer(this.pipeBoardState);
+	}
+	/*
+	 * getSolutionFromServer will you send the pipeBoardState init 
+	 * board and will receive back the solution.
+	 */
+	private char[][] getSolutionFromServer(char[][] boardToSendTotheServer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	@Override
 	public void solveGame(char[][] gameData) {
@@ -19,4 +52,19 @@ public class PipeGameModel extends Observable implements gameModel {
 		return this.gameSolution;
 	}
 	
+	/*
+	 * isSolution responsible on return true or false is the 
+	 * saved state of the pipe game is correct.
+	 * It will run char char and compare Solution
+	 * to saved state.
+	 */
+
+	public boolean isSolution() {
+		for (int i = 0 ; i < n ; ++i)
+			for (int j =0 ; j < n ; ++j) {
+				if (pipeBoardState[i][j] != gameSolution[i][j])
+					return false;
+			}
+		return true;
+	}
 }
